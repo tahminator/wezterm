d68725442dbadac8fb519682839c6e38e135657a
Tahmid Ahmed <tahmid@tahmid.io>
2025-05-27 02:43:58 -0400

    Use PowerShell on Windows

--- wezterm.lua
+++ wezterm.lua
@@ -88,7 +88,7 @@ local config = {
 }
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_prog = { "pwsh.exe", "-NoLogo" }
+	config.default_prog = { "powershell" }
 end
 
 -- Fullscreen mode on launch.
