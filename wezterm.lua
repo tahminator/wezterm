e64c978e943f18744bcf055a8b783c3dcfa46ed3
Tahmid Ahmed <tahmid@tahmid.io>
2025-06-01 20:20:53 -0400

    Add background blur + translucency on MacOS

--- wezterm.lua
+++ wezterm.lua
@@ -45,6 +45,8 @@ local config = {
 	use_fancy_tab_bar = false,
 	window_decorations = "RESIZE",
 	hide_tab_bar_if_only_one_tab = true,
+	window_background_opacity = 0.8,
+	macos_window_background_blur = 20,
 
 	front_end = "WebGpu",
 	max_fps = 240,
